a9b1863e2272ef8c4e311c97e998309e7de70a6c
ahvvad <ahvadd.k@gmail.com>
2025-02-10 00:28:40 +0200

    ToDo List to finshung the app

--- lib/main.dart
+++ lib/main.dart
@@ -28,3 +28,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// ToDo: change the UI of the app
+// ToDo: change the font to => GoogleFonts.barlowTextTheme(),
+// ToDo: change the images to Awesome icons [ location-arrow / gas-pump / battery-three-quarters / arrow-back of the maps pages ]
+// ToDo: refactor the code
+// ToDo: animate the  maps
+// ToDo: add a Clean-Arc & BLoC pattern and Firebase database implementation
